795a03b843f59255f63a46d44f9766020193ece3
Sreepathi Pai <sreepai@ices.utexas.edu>
2015-09-10 18:44:07 -0500

    Add opdb.py, abstract cfg files, inputprops is broken

--- inputdb.py
+++ inputdb.py
@@ -8,29 +8,81 @@ import common
 import fnmatch
 import inputprops
 from core import Input
+from opdb import ObjectPropsCFG
+
+class InputDBcfg(ObjectPropsCFG):
+    def __init__(self, filename, inpproc = None, bt = None):
+        super(InputDBcfg, self).__init__(filename, "bmktest2", ["2"])
+        self.inpproc = inpproc
+        self.bt = bt
+
+        self.unserialize_input = None
+        self.serialize_input = None
+
+        if self.inpproc:
+            inpproc = common.load_py_module(self.inpproc)
+            if 'unserialize_input' in inpproc:
+                self.unserialize_input = inpproc['unserialize_input']
+
+            if 'serialize_input' in inpproc:
+                self.serialize_input = inpproc['serialize_input']                
+
+    def init(self, basepath):
+        self.meta = dict([('version', "2"), ('basepath', basepath)])
+
+    def post_load(self):
+        if self.bt is not None:
+            self.bt.update(self.meta)
+
+        basepath = self.meta['basepath']
+
+        for s in self.objects:
+            e = self.objects[s]
+            if self.unserialize_input:
+                e = self.unserialize_input(e, basepath)
+
+                e['file'] = os.path.join(basepath, e['file'])
+
+        return True
+
+    def unparse_section(self, section):
+        if self.serialize_input:
+            self.serialize_input(section)
+
+        if 'file' in section:
+            l = len(self.meta['basepath']) 
+            if section['file'][:l] == self.meta['basepath']:
+                section['file'] = section['file'][l+1:]
+
+
+        return section
 
 class InputDB(object):
     def __init__(self, cfgfile, inpproc = None, inputprops = None):
         self.cfg = cfgfile
         self.inpproc = inpproc
         self.inputprops = inputprops
+        self.inputdb_cfg = InputDBcfg(self.cfg, self.inpproc, {})
 
     def get_alt_format(self, name, fmt):
         if name in self.n2i:
             for x in self.n2i[name]:
                 if x.props.format == fmt:
                     return x
-        
+
     def load(self):
-        bt = {}
-        self.inputdb = read_cfg_file(self.cfg, self.inpproc, bt)
+        if not self.inputdb_cfg.load():
+            print >>sys.stderr, "Unable to load!"
+            return False
+
+        bt = self.inputdb_cfg.bt
 
         if self.inputprops is not None:
             # not .props as Properties!
             self.props = inputprops.read_prop_file(self.inputprops, bt)
             inputprops.apply_props(self.inputdb, self.props)
 
-        self.inputdb = [Input(i, self) for i in self.inputdb]
+        self.inputdb = [Input(i, self) for i in self.inputdb_cfg]
         self.inpnames = set([i.get_id() for i in self.inputdb])
 
         self.n2i = dict([(n, list()) for n in self.inpnames])
@@ -39,61 +91,7 @@ class InputDB(object):
 
     def __iter__(self):
         return iter(self.inputdb)
-
-def read_cfg_file(cfgfile, inpproc = None, bmktest2 = None):
-    unserialize_input = None
-
-    if inpproc:
-        inpproc = common.load_py_module(inpproc)
-        if 'unserialize_input' in inpproc:
-            unserialize_input = inpproc['unserialize_input']
-
-    x = ConfigParser.SafeConfigParser()
-
-    out = []
-    with open(cfgfile, "rb") as f:
-        x.readfp(f)
-
-        v = x.getint("bmktest2", "version")
-        if v != 2:
-            printf >>sys.stderr, "Unknown version: %s" % (v,)
-            return None
-
-        basepath = x.get("bmktest2", "basepath")
-
-        for s in x.sections():
-            if s == "bmktest2": 
-                if bmktest2 is not None:
-                    bmktest2.update(x.items(s))
-
-                continue
-
-            e = dict(x.items(s))
-            if unserialize_input:
-                e = unserialize_input(e, basepath)
-
-            e['file'] = os.path.join(basepath, e['file'])
-
-            out.append(e)
-
-    return out
-
-def write_cfg_file(cfgfile, basepath, entries):
-    x = ConfigParser.SafeConfigParser()
-    
-    x.add_section("bmktest2")
-    x.set("bmktest2", "version", "2")
-    x.set("bmktest2", "basepath", basepath)
-    
-    for e in entries:
-        sec = e['file']
-        x.add_section(sec)
-        for p in e:
-            x.set(sec, p, e[p])
-
-    with open(cfgfile, 'wb') as f:
-        x.write(f)
-        
+       
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="Prepare an inputs database")
     p.add_argument("--glob", help="Glob")
@@ -105,18 +103,17 @@ if __name__ == "__main__":
     args = p.parse_args()
     inpproc = common.load_py_module(args.inpproc)
 
-    existing_files = set()
     if args.update:
         idb = InputDB(args.dbfile, args.inpproc)
         idb.load()
-        existing_files = [x.props.file for x in idb]
-        
-    print existing_files
+        basepath = idb.inputdb_cfg.meta['basepath']
+        print >>sys.stderr, "using basepath from file: %s" % (basepath,)
+    else:
+        idb = InputDB(args.dbfile, args.inpproc)
+        basepath = args.basepath
+        idb.inputdb_cfg.init(basepath)
 
     describe_input = inpproc['describe_input']
-
-    basepath = args.basepath
-    dbfile = args.dbfile
 
     out = []
     for root, dirnames, filenames in os.walk(basepath):
@@ -129,15 +126,13 @@ if __name__ == "__main__":
             x = describe_input(root, f, rp)
             if x:
                 x['file'] = os.path.join(rp, f)
-                fp = os.path.join(basepath, x['file'])
-
-                if fp not in existing_files:
-                    print fp
-                    out.append(x)
-
+                if x['file'] not in idb.inputdb_cfg.objects:
+                    print >>sys.stderr, x['file']
+                    idb.inputdb_cfg.objects[x['file']] = x
 
     if args.update:
-        write_cfg_file(dbfile + ".update", basepath, out)
+        #TODO: check for basepath changes
+        idb.inputdb_cfg.save(args.dbfile + ".update")
     else:
-        write_cfg_file(dbfile, basepath, out)
+        idb.inputdb_cfg.save(args.dbfile)
 
---
+++ opdb.py
@@ -0,0 +1,94 @@
+import ConfigParser
+from collections import OrderedDict
+
+def cfg_get(fn, section, key, default=None):
+    try:
+        v = fn(section, key)
+        return v
+    except ConfigParser.NoOptionError:
+        return default
+
+class ObjectProps(object):
+    pass
+
+class ObjectPropsCFG(ObjectProps):
+    """Read/Write a .cfg file as a object property file.
+    
+       Sections names indicate objects, section keys indicate properties."""
+
+    def __init__(self, filename, fmt, acceptable_versions):
+        self.filename = filename
+        self.fmt = fmt
+        self.acceptable_versions = acceptable_versions
+        self.meta = None
+        self.objects = OrderedDict()
+
+    def check_version(self, version):
+        return version in self.acceptable_versions
+
+    def update_props(self, props):
+        return props
+
+    def parse_section(self, cfg, section): 
+        d = OrderedDict(cfg.items(section))
+        d = self.update_props(d)
+        return d
+
+    def unparse_section(self, section):
+        return section
+    
+    def post_load(self):
+        return True
+
+    def load(self):
+        x = ConfigParser.SafeConfigParser()
+
+        out = OrderedDict()
+        with open(self.filename, "rb") as f:
+            x.readfp(f)
+
+            v = cfg_get(x.get, self.fmt, "version")
+
+            if not self.check_version(v):
+                av = [str(v) for v in self.acceptable_versions]
+                if v:
+                    print >>sys.stderr, "Unknown version: %s (acceptable: %s)" % (v, ", ".join(av))
+                else:
+                    print >>sys.stderr, "Unable to determine version (acceptable: %s)" % (", ".join(av))
+                
+            for s in x.sections():
+                if s == self.fmt:
+                    self.meta = self.parse_section(x, s)
+                else:
+                    if s in out:
+                        print >>sys.stderr, "Warning: Duplicate section '%s', overwriting" % (s,)
+
+                    out[s] = self.parse_section(x, s)
+
+            self.objects = out
+            return self.post_load()
+
+        return False
+
+    def save(self, fn = None):
+        def write_items(cfg, section, items):
+            for k, v in items.iteritems():
+                cfg.set(section, k, v)
+
+        x = ConfigParser.SafeConfigParser()
+        
+        assert self.filename or fn, "Both filename and fn cannot be empty."
+        if not fn: fn = self.filename
+
+        x.add_section(self.fmt)
+        write_items(x, self.fmt, self.unparse_section(self.meta))
+
+        for s in self.objects:
+            x.add_section(s)
+            write_items(x, s, self.unparse_section(self.objects[s]))
+
+        with open(fn, "wb") as f:
+            x.write(f)
+        
+    def __iter__(self):
+        return iter(self.objects.itervalues())
