ab5414098456a77baf20994281f25126bdc7403b
Sreepathi Pai <sreepai@ices.utexas.edu>
2015-09-14 16:10:17 -0500

    convert: Output make rules, needs more work

--- convert.py
+++ convert.py
@@ -5,6 +5,21 @@ import ConfigParser
 import argparse
 from extras import *
 import logging
+import opdb
+import os
+import re
+import convgraph
+
+def gen_xform_fn(srcname, dstname):
+    src_re = re.compile(srcname)
+
+    def f(s):
+        return src_re.sub(dstname, s)
+
+    return f
+
+class ConvSpec(opdb.ObjectPropsCFG):
+    pass
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(levelname)-8s %(name)-10s %(message)s')
@@ -24,8 +39,64 @@ if not loaded:
 else:
     basepath, binspecs, l = loaded
 
-rspecs = l.get_run_specs()
-#rspecs.sort(key=lambda x: x.bid)
+convspec = l.config.get_var('convspec', None)
+if not convspec:
+    log.error("No 'convspec' in config file")
+    sys.exit(1)
 
+cs = ConvSpec(os.path.join(l.config.metadir, convspec), "bmk2-convspec", ["2"])
+if not cs.load():
+    log.error("Unable to read config file")
+    sys.exit(1)
+
+all_types = set()
+conv = {}
+for n, s in cs.objects.iteritems():    
+    convgraph.register_conversion(s['src'], s['dst'], 
+                                  gen_xform_fn(s['srcname'],
+                                               s['dstname']))
+
+    all_types.add(s['src'])
+    all_types.add(s['dst'])
+
+    conv[(s['src'], s['dst'])] = n
+
+targets = []
+out = []
+rspecs = l.get_run_specs()
 for rs in rspecs:
-    print rs
+    src, srcty, dst, dstty = rs.args
+    src, srcty, dst, dstty = src[0], srcty[0], dst[0], dstty[0]
+
+    if srcty not in all_types:
+        log.error("Conversion from %s not supported" % (srcty,))
+        sys.exit(1)
+
+    if dstty not in all_types:
+        log.error("Conversion to %s not supported" % (dstty,))
+        sys.exit(1)
+
+    c = convgraph.get_conversion(src, srcty, dst, dstty, {})
+    if not c:
+        log.error("Conversion from %s to %s not supported" % (srcty, dstty))
+        sys.exit(1)
+
+    for cmd, fs, fsty, ds, dsty in c:
+        assert cmd == "convert_direct", "Unsupported: %s" % (cmd,)
+        assert (fsty, dsty) in conv, "Planner got it wrong: %s -> %s unsupported" % (fst, dsty)
+
+        if os.path.exists(ds):
+            continue
+
+        cmd = cs.objects[conv[(fsty, dsty)]]['cmd']
+        cmd = cmd.format(src = fs, dst=ds)
+
+        targets.append(ds)
+        out.append("""
+{dst}: {src}
+\t{cmd}""".format(src=fs, dst=ds, cmd=cmd))
+
+if len(targets):
+    print "all: %s" % (" ".join(targets))
+    print "\n".join(out)
+
--- convgraph.py
+++ convgraph.py
@@ -2,38 +2,25 @@
 
 from pyhop import *
 
-def gr_to_dimacs(state, src, dst):
-    return "%s ->gr_to_dimacs %s" % (src, dst)
+conversions = {}
 
-def dimacs_to_gr(state, src, dst):
-    return "%s ->dimacs_to_gr %s" % (src, dst)
+def register_conversion(src, dst, fn_xform):
+    assert (src, dst) not in conversions, "Duplicate conversion (%s, %s)" % (src, dst)
 
-def dimacs_to_other(state, src, dst):
-    return "%s ->dimacs_to_other %s" % (src, dst)
-
-conversions = {('binary/gr', 'text/dimacs'): gr_to_dimacs,
-               ('text/dimacs', 'binary/gr'): dimacs_to_gr,
-               ('text/dimacs', 'other/format1'): dimacs_to_other,
-               ('other/format1', 'other/format'): dimacs_to_other}
-
-# graph file converter
-
-# exists format/A
-# [B does not exist, A exists] convert A to B [A exists, B exists]
-# converter A/fmt1 to B/fmt2
+    conversions[(src, dst)] = fn_xform
 
 def convert_direct(state, a, fmt_a, b, fmt_b):
     # we must have a direct converter
     if (fmt_a, fmt_b) not in conversions:
-        print "no direct conversion"
+        #print "no direct conversion"
         return False
 
     if state.files[fmt_a] != a and state.existing[fmt_a] != a:
-        print "src does not exist"
+        #print "src does not exist"
         return False
 
     if state.files[fmt_b] == b:
-        print "dst exists"
+        #print "dst exists"
         return False
     
     state.files[fmt_b] = b
@@ -57,7 +44,11 @@ def convert_via(state, a, fmt_a, b, fmt_b):
     else:
         for s, d in conversions:
             if s == fmt_a and state.files[d] is None:
-                via = "%s_%s" % (a, d)
+                via = conversions[(s, d)](a)
+
+                if via == a: # did not match regex
+                    return False
+
                 return [('convert', a, fmt_a, via, d),
                         ('convert', via, d, b, fmt_b)]
         
@@ -65,24 +56,44 @@ def convert_via(state, a, fmt_a, b, fmt_b):
 
 declare_methods('convert', convert_from_existing, convert_via)
 
-start_file = 'a'
-start_file_fmt = 'binary/gr'
+def get_conversion(start, start_ty, end, end_ty, existing, verbose=0):
+    s = State('initial')
+    
+    # do we need existing?
+    s.existing = {}
+    s.files = {}
 
-s = State('initial')
-s.existing = {}
-s.files = {}
+    for f1, f2 in conversions.keys():
+        s.files[f1] = None
+        s.files[f2] = None
 
-for f1, f2 in conversions.keys():
-    s.files[f1] = None
-    s.files[f2] = None
+    for k, v in existing.keys():
+        s.existing[k] = v
 
-s.files[start_file_fmt] = start_file
+    s.files[start_ty] = start
 
-s.existing[start_file_fmt] = start_file
-s.existing['other/format1'] = 'c'
+    x = pyhop(s, [('convert', start, start_ty, end, end_ty)], verbose=verbose)
+    return x
 
-x = pyhop(s, [('convert', 'a', 'binary/gr', 'b', 'other/format')], verbose=2)
+if __name__ == "__main__":
+    start_file = 'a'
+    start_file_fmt = 'binary/gr'
 
-if not x:
-    print "conversion is unsupported"
+    s = State('initial')
+    s.existing = {}
+    s.files = {}
 
+    for f1, f2 in conversions.keys():
+        s.files[f1] = None
+        s.files[f2] = None
+
+    s.files[start_file_fmt] = start_file
+
+    s.existing[start_file_fmt] = start_file
+    s.existing['other/format1'] = 'c'
+
+    x = pyhop(s, [('convert', 'a', 'binary/gr', 'b', 'other/format')], verbose=2)
+    if not x:
+        print "conversion is unsupported"
+
+        
