febb5285c81d123227c4966324818a1eb8b62ed3
Sreepathi Pai <sreepai@ices.utexas.edu>
2015-10-20 15:33:00 -0500

    Fix bug in implicit use of experiment as key

--- ps/norm.py
+++ ps/norm.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description="Summarize performance data", fromf
 parser.add_argument("base", help="Base file")
 parser.add_argument("input", help="Input file")
 parser.add_argument("-m", dest="metric", help="Metric to normalize")
-parser.add_argument("-k", dest="key", action="append", default=['experiment'])
+parser.add_argument("-k", dest="key", action="append", default=[])
 parser.add_argument("--rk", dest="right_keys", action="append", default=[], help="Retain right keys")
 parser.add_argument("--rsuffix", dest="right_suffix", default="_right")
 parser.add_argument("-o", dest="output", metavar="FILE", 
@@ -21,7 +21,7 @@ parser.add_argument("-o", dest="output", metavar="FILE",
 args = parser.parse_args()
 
 t = pd.read_csv(args.input)
-if len(args.key) == 1:
+if len(args.key) == 0:
     args.key += cfg.get_key()
 
 common_keys = set(args.key) & set(args.right_keys)
@@ -31,6 +31,7 @@ if common_keys != set():
     
 t = pd.read_csv(args.input)
 b = pd.read_csv(args.base)
+#print b[args.key + [args.metric] + args.right_keys]
 m = t.merge(b[args.key + [args.metric] + args.right_keys], 'left', on=args.key, suffixes=('', args.right_suffix))
 m[args.metric + "_norm"] = m[args.metric] / m[args.metric + "_right"]
 m.to_csv(args.output,index=False)
