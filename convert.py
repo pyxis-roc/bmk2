5cc35489adecf3cb5f5b012567441368d4799369
Sreepathi Pai <sreepai@ices.utexas.edu>
2015-10-29 11:56:59 -0500

    Add sconvert.py for single conversions, make convert.py use sconvert.py under-the-hood.

--- convert.py
+++ convert.py
@@ -8,18 +8,7 @@ import logging
 import opdb
 import os
 import re
-import convgraph
-
-def gen_xform_fn(srcname, dstname):
-    src_re = re.compile(srcname)
-
-    def f(s):
-        return src_re.sub(dstname, s)
-
-    return f
-
-class ConvSpec(opdb.ObjectPropsCFG):
-    pass
+import sconvert
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(levelname)-8s %(name)-10s %(message)s')
@@ -46,24 +35,12 @@ if not convspec:
     log.error("No 'convspec' in config file")
     sys.exit(1)
 
-cs = ConvSpec(os.path.join(l.config.metadir, convspec), "bmk2-convspec", ["2"])
-if not cs.load():
-    log.error("Unable to read config file")
+cs = sconvert.load_convspec(os.path.join(l.config.metadir, convspec))
+if not cs:
     sys.exit(1)
 
-all_types = set()
-conv = {}
-for n, s in cs.objects.iteritems():    
-    convgraph.register_conversion(s['src'], s['dst'], 
-                                  gen_xform_fn(s['srcname'],
-                                               s['dstname']))
+all_types, conv = sconvert.init_convgraph(cs)
 
-    all_types.add(s['src'])
-    all_types.add(s['dst'])
-
-    conv[(s['src'], s['dst'])] = n
-
-targets = []
 out = []
 rspecs = l.get_run_specs()
 for rs in rspecs:
@@ -79,65 +56,14 @@ for rs in rspecs:
         if os.path.exists(alt.props.file):
             # sometimes alt.props.file may only exist in the database
             exists[alt.props.format] = alt.props.file
-
-    # might be useful for a copy?
-    if dstty in exists:
-        del exists[dstty]
-
-    #print exists
-
-    if srcty not in all_types:
-        log.error("Conversion from %s not supported" % (srcty,))
-        sys.exit(1)
-
-    if dstty not in all_types:
-        log.error("Conversion to %s not supported" % (dstty,))
-        sys.exit(1)
-
-    if dst == "@output":
-        dst = None
-
-    # silently skip destinations that already exist in database and on disk
-    if dst and os.path.exists(dst):
-        # we're also abandoning any intermediate files ...
-        # TODO: the planner should do this...
+            
+    cmds = sconvert.convert_one(src, srcty, dst, dstty, all_types, conv, exists)
+    if cmds is None:
         continue
 
-    c = convgraph.get_conversion(src, srcty, dst, dstty, exists, args.verbose)
-    if not c:
-        log.error("Conversion from %s to %s not supported" % (srcty, dstty))
-        sys.exit(1)
+    out.append(cmds)
 
-    if False:
-        print >>sys.stderr, c
-
-    if dst is None:
-        dst = c[-1][3]
-
-    # skip destinations that only exist on disk but not in database
-    if os.path.exists(dst):
-        log.info("Destination `%s' already exists, you need to update inputdb." % (dst,))
-        continue
-
-    for cmd, fs, fsty, ds, dsty in c:
-        assert cmd == "convert_direct", "Unsupported: %s" % (cmd,)
-        assert (fsty, dsty) in conv, "Planner got it wrong: %s -> %s unsupported" % (fst, dsty)
-        #print c
-
-        if os.path.exists(ds):
-            continue
-
-        cmd = cs.objects[conv[(fsty, dsty)]]['cmd']
-        cmd = cmd.format(src = fs, dst=ds, verbose=1)
-
-        out.append("""
-{dst}: {src}
-\t{cmd}""".format(src=fs, dst=ds, cmd=cmd))
-
-    targets.append(dst) # dst
-
-if len(targets):
+if len(out):
     f = open(args.output, "w")
-    print >>f, "all: %s" % (" ".join(targets))
-    print >>f, "\n".join(out)
+    sconvert.to_makefile(out)
     f.close()
