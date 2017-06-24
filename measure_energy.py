c19e28340a895996d5a826d7d8bebdf81c754432
Sreepathi Pai <sreepai@ices.utexas.edu>
2017-06-24 15:05:58 -0500

    Handle RAPL wraparound.

--- measure_energy.py
+++ measure_energy.py
@@ -15,6 +15,9 @@ import sys
 import os
 import subprocess
 import glob
+import threading
+
+INTERVAL_S = 15
 
 def get_rapl_files():
     dom = glob.glob("/sys/class/powercap/intel-rapl:*")
@@ -25,6 +28,11 @@ def get_rapl_files():
         f = os.path.join(d, "energy_uj")
         if os.path.exists(f):
             out[dd] = f
+
+        f = os.path.join(d, "max_energy_range_uj")
+        if os.path.exists(f):
+            out["max_" + dd] = f
+
 
     return out
 
@@ -37,10 +45,40 @@ def read_rapl_power(rapl_files):
 
     return out
 
-def calc_power(bef, aft):
+def periodic_power():
+    global TIMER
+    POWER.append(read_rapl_power(rf))
+    TIMER = threading.Timer(INTERVAL_S, periodic_power)
+    TIMER.start()
+
+def count_wraparound(nums, key = None):
+    prev = None
+    wrap = 0
+
+    for n in nums:
+        if key:
+            n = key(n)
+
+        if prev is None:
+            prev = n
+        else:
+            if prev > n:
+                wrap += 1
+
+    return wrap
+
+def calc_power(POWER):
+    k = [kk for kk in POWER[0].keys() if kk[:4] != 'max_']
+
     out = {}
-    for k in bef:
-        out[k] = aft[k] - bef[k]
+    for kk in k:
+        wraps = count_wraparound(POWER, key = lambda x: x[kk])
+        
+        bef = POWER[0][kk]
+        aft = POWER[-1][kk] + wraps * POWER[-1]["max_" + kk]
+
+        out[kk] = aft - bef
+        out[kk+":wraps"] = wraps
 
     return out
 
@@ -52,12 +90,22 @@ cmdline = sys.argv[1:]
 
 rf = get_rapl_files()
 if len(rf):
-    bef = read_rapl_power(rf)
-    subprocess.check_call(cmdline)
-    aft = read_rapl_power(rf)
-    p = calc_power(bef, aft)
+    POWER = []
+    TIMER = threading.Timer(INTERVAL_S, periodic_power)
+    POWER.append(read_rapl_power(rf))
+    TIMER.start()
+
+    proc = subprocess.Popen(cmdline)
+    proc.wait()
+
+    TIMER.cancel()
+    POWER.append(read_rapl_power(rf))
+
+    p = calc_power(POWER)
     for k in p:
         print "INSTR", k, p[k] # micro joules
+
+    sys.exit(proc.returncode)
 else:
     print >>sys.stderr, "Did not find RAPL power counters (/sys/class/powercap/intel-rapl*)"
     sys.exit(1)
